0c57f52936a2d6be32b909bfa147f9d32510828b
Melissa Rose <123melissa321@gmail.com>
2022-09-25 21:57:37 +0100

    Toggle 2D/3D

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import P5Component from "./P5Component";
 import { greet } from "./utils/greet";
 
 function App(): JSX.Element {
-  const [page, setPage] = useState<'3d-home'|'2d-home'|'todo-app'|'restaurant'|'playlist-converter'|'pokemon-top-trumps'>('3d-home')
+  const [page, setPage] = useState<'home'|'todo-app'|'restaurant'|'playlist-converter'|'pokemon-top-trumps'>('home')
   const [mode, setMode] = useState<'2D'|'3D'>('3D');
   return (
     <>
       <h1>{greet("World")}</h1>
-      {page==='3d-home' && 
-      
+      {page==='home' &&
+      <button type='button' onClick={() => {mode==='3D'? setMode('2D'): setMode('3D')}}>Toggle 2D/3D</button>
+      }
+      {page==='home' && mode==='3D' &&
       <P5Component setPage={setPage}/>}
       {page==='todo-app' && <TodoApp mode={mode} setPage={setPage}/>}
-      
+      {page==='home' && mode==='2D' && <h1>2d</h1>}
     </>
   );
 }
--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,13 +1,13 @@
 interface ITodoAppProps {
     mode: '2D'|'3D';
-    setPage: React.Dispatch<React.SetStateAction<"3d-home" | "2d-home" | "todo-app" | "restaurant" | "playlist-converter" | "pokemon-top-trumps">>;
+    setPage: React.Dispatch<React.SetStateAction<"home" | "todo-app" | "restaurant" | "playlist-converter" | "pokemon-top-trumps">>;
 }
 
 function TodoApp({mode, setPage}: ITodoAppProps): JSX.Element {
     return (
         <div id='todo-app'>
             <h1>Todo list app</h1>
-            <button type='button' onClick={() => {mode==='3D' ? setPage('3d-home') : setPage('2d-home')}}>Back</button>
+            <button type='button' onClick={() => setPage('home')}>Back</button>
             <button type='button'>Link to app</button>
             <button type='button'>Frontend repository</button>
             <button type='button'>Backend repository</button>
