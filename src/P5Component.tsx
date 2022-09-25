eb539d59a2e9427218218e58dfc6d72d925b35c8
Melissa Rose <123melissa321@gmail.com>
2022-09-25 14:19:44 +0100

    Added text plane

--- src/P5Component.tsx
+++ src/P5Component.tsx
@@ -1,8 +1,11 @@
 import p5 from 'p5'
 import { useEffect, useRef } from 'react';
 
-let myCamera;
-const myPosition = {x: 0, y: 0, z: 0, direction: 0}
+let myCamera: p5.Camera;
+const myPosition = {x: 0, y: 0, z: 0, direction: 0};
+let cameraChoice: 'chase'|'far' = 'chase';
+let instructions: p5.Graphics;
+
 
 function sketch(p: p5) {
     // p is a reference to the p5 instance this sketch is attached to
@@ -16,6 +19,8 @@ function sketch(p: p5) {
 	myCamera = p.createCamera();
 	myCamera.setPosition(100, -200, 400);
 	myCamera.lookAt(0, 0, 0);
+    instructions = p.createGraphics(200, 200);
+    instructions.textSize(75);
     }
 
     p.draw = function() {
@@ -29,13 +34,33 @@ function sketch(p: p5) {
 
 	//Add a little light evenly to ALL surfaces.  Not too much or we'll see no shadow
 	p.ambientLight(180, 150, 150);
-        p.push();
-        p.translate(myPosition.x, myPosition.y, myPosition.z);
-        p.fill(204, 102, 0);
-        p.sphere(2);
-        p.pop();
-        moveSelf();
+    p.push();
+    p.translate(myPosition.x, myPosition.y, myPosition.z);
+    p.fill(204, 102, 0);
+    p.sphere(2);
+    p.pop();
+    moveSelf();
+    moveCamera();
+        keyPressed();
         // your draw code here
+        drawInstructions();
+    }
+
+    function drawInstructions(){
+        p.push();
+        p.translate(0, 0, 0);
+        p.fill(0, 0, 0)
+        instructions.background(255);
+  instructions.text('hello!', 0, 100);
+  //pass image as texture
+  p.texture(instructions);
+  p.plane(50);
+  p.pop();
+        // p.push();
+        // p.fill(204, 102, 0);
+        // p.translate(20, 0, 0);
+        // p.text('Instructions', 0, 0);
+        // p.pop();
     }
 
     function moveSelf(){
@@ -55,6 +80,27 @@ function sketch(p: p5) {
             myPosition.x -= 3*p.cos(myPosition.direction);
             myPosition.z -= 3*p.sin(myPosition.direction);
         }	
+        }
+
+        function moveCamera() {
+            if (cameraChoice === 'chase') {
+                myCamera.setPosition(myPosition.x - 50*p.cos(myPosition.direction), -30, myPosition.z - 50*p.sin(myPosition.direction));
+                myCamera.lookAt(myPosition.x, 0, myPosition.z);		
+            } else if (cameraChoice === 'far') {
+                    myCamera.setPosition(myPosition.x, -400, myPosition.z);
+                    myCamera.lookAt(myPosition.x - 50*p.cos(myPosition.direction), myPosition.y, myPosition.z - 50*p.sin(myPosition.direction));
+            }
+        }
+        
+        function keyPressed() {
+            if (p.key === 'c') {
+                if (cameraChoice === 'chase') {
+                    cameraChoice = 'far'
+                } else {
+                    // myCamera.lookAt(myPlayer.x, 0, myPlayer.z);
+                    cameraChoice = 'chase';	
+                }
+            }
         }
     
 }
