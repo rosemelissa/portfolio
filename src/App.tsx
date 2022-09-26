a4a124e119124bf8fa672e35cf0c5d65ab525e5e
Melissa Rose <123melissa321@gmail.com>
2022-09-26 16:12:56 +0100

    Added pages for all 4 projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import PlaylistConverter from "./components/PlaylistConverter";
+import PokemonTopTrumps from "./components/PokemonTopTrumps";
+import Restaurant from "./components/Restaurant";
 import TodoApp from "./components/TodoApp";
 import Home2D from "./Home2D";
 import P5Component from "./P5Component";
@@ -28,6 +31,9 @@ function App(): JSX.Element {
       )}
       {page === "home" && mode === "3D" && <P5Component setPage={setPage} />}
       {page === "todo-app" && <TodoApp mode={mode} setPage={setPage} />}
+      {page === "restaurant" && <Restaurant mode={mode} setPage={setPage} />}
+      {page === "playlist-converter" && <PlaylistConverter mode={mode} setPage={setPage} />}
+      {page === "pokemon-top-trumps" && <PokemonTopTrumps mode={mode} setPage={setPage} />}
       {page === "home" && mode === "2D" && <Home2D setPage={setPage} />}
     </>
   );
---
+++ src/components/Restaurant.tsx
@@ -0,0 +1,31 @@
+interface IRestaurantProps {
+  mode: "2D" | "3D";
+  setPage: React.Dispatch<
+    React.SetStateAction<
+      | "home"
+      | "todo-app"
+      | "restaurant"
+      | "playlist-converter"
+      | "pokemon-top-trumps"
+    >
+  >;
+}
+
+function Restaurant({ mode, setPage }: IRestaurantProps): JSX.Element {
+  return (
+    <div id="todo-app">
+      <h1>Restaurant</h1>
+      <button type="button" onClick={() => setPage("home")}>
+        Back
+      </button>
+      <button type="button">Link to app</button>
+      <button type="button">Frontend repository</button>
+      <button type="button">Backend repository</button>
+      <p>Description</p>
+      <p>Images</p>
+      <p>Other documentation links</p>
+    </div>
+  );
+}
+
+export default Restaurant;
