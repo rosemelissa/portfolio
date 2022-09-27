e0f1b86fd4011597727584026aba89021c188017
Melissa Rose <123melissa321@gmail.com>
2022-09-27 12:26:41 +0100

    Finished TodoApp page with images

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,3 +1,10 @@
+import TodoNewProject from '../Images/TodoApp/TodoNewProject.png';
+import TodoColourCoding from '../Images/TodoApp/TodoColourCoding.png';
+import TodoEdit from '../Images/TodoApp/TodoEdit.png';
+import TodoFilter from '../Images/TodoApp/TodoFilter.png';
+import ExampleTodoList from '../Images/TodoApp/ExampleTodoList.png';
+
+
 interface ITodoAppProps {
   mode: "2D" | "3D";
   setPage: React.Dispatch<
@@ -48,22 +55,48 @@ function TodoApp({ mode, setPage }: ITodoAppProps): JSX.Element {
         </p>
       </div>
       <div className="project-main-content">
-        <div className="text-image-couple">
-          <p>Description</p>
+          <h3 className='project-tagline'>Fullstack todo app using PostgreSQL, Express, Node.js, React, HTML, and CSS</h3>
+          
+            <img id='example-todo-list'
+            src={ExampleTodoList}
+            alt="Todo app landing page"
+            />
+          
+          <h3>Key features:</h3>
+          <ul>
+            <li>Make, edit, and delete projects</li>
+            <li>Make, edit, and delete todos</li>
+          <li>Filter todos</li>
+          <li>View todo-status at a glance through colour coding</li>
+          </ul>
+          
+        <div className="project-image-grid">
           <img
-            src="https://www.musicmundial.com/en/wp-content/uploads/2022/06/Jungkook-finally-fulfills-one-of-his-dreams-after-going-on-hiatus-with-BTS.jpg"
-            alt="jungkook"
+            src={TodoNewProject}
+            alt="Making a new project"
+            />
+<img
+            src={TodoEdit}
+            alt="Editing a todo"
           />
-        </div>
-        <div className="text-image-couple">
-          <p>Description2</p>
+
+
+
           <img
-            src="https://www.musicmundial.com/en/wp-content/uploads/2022/06/Jungkook-finally-fulfills-one-of-his-dreams-after-going-on-hiatus-with-BTS.jpg"
-            alt="jungkook"
+            src={TodoFilter}
+            alt="Filtering todos"
           />
+          <img
+            src={TodoColourCoding}
+            alt="Todos are colour coded"
+          />
+ 
+
+
+            
+          
         </div>
-      </div>
-      <p>Other documentation links</p>
+        </div>
     </div>
   );
 }
