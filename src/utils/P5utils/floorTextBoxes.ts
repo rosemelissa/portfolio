cdac9810a5bc3e50bb5a05595b7040631df9871a
Melissa Rose <123melissa321@gmail.com>
2022-09-29 10:05:15 +0100

    Removed welcome text box

--- src/utils/P5utils/floorTextBoxes.ts
+++ src/utils/P5utils/floorTextBoxes.ts
@@ -1,16 +1,16 @@
 import { ITextBox } from "../../interfaces";
 
 export const floorTextBoxes: ITextBox[] = [
-  {
-    position: { x: 100, y: 100, z: 0, direction: 0 },
-    color: { v1: 0, v2: 0, v3: 0 },
-    message: "Welcome! Use arrow keys to move",
-    textSize: 30,
-    plane: {
-      width: 300,
-      height: 100,
-    },
-  },
+  // {
+  //   position: { x: 100, y: 100, z: 0, direction: 0 },
+  //   color: { v1: 0, v2: 0, v3: 0 },
+  //   message: "Welcome! Use arrow keys to move",
+  //   textSize: 30,
+  //   plane: {
+  //     width: 300,
+  //     height: 100,
+  //   },
+  // },
   {
     position: { x: 400, y: 100, z: -200, direction: 0 },
     color: { v1: 0, v2: 0, v3: 0 },
